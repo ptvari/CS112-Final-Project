52d80f49d0cf94e76278d5ede1ad2301395dbd97
aris <adrian.teniente141@gmail.com>
2026-04-30 15:18:17 -0700

    yo

--- CourseClass.py
+++ CourseClass.py
@@ -3,8 +3,14 @@ This portion is for the class function
 """
 
 class Course:
-    def __init__(self,course_name,score):
+    def __init__(self,course_name):
         self.course_name=course_name
-        #assignment_name, pts earned, pts_poss
-        self.assignments={}
-        self.score=score
+        self.assignments=[]
+
+    def add_assignment(self, name, score, points):
+        assignment={
+            "name": name,
+            "score": score,
+            "points": points
+        }
+        self.assignments.append(assignment)
