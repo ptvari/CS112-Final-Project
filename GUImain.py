feb437a8b23bf72e06880df4bf4e8673a4213533
aris <adrian.teniente141@gmail.com>
2026-05-06 14:33:16 -0700

    update

--- GUImain.py
+++ GUImain.py
@@ -44,7 +44,7 @@ entryName.pack()
 entryScore.pack()
 entryPoints.pack()
 
-#gr
+#grab the selected course assignments from option box
 def get_selected_course():
     return courses[selected_course.get()]
 
@@ -53,12 +53,59 @@ def add_assignment_gui():
     course = get_selected_course()
 
     name = entryName.get()
-    score = int(entryScore.get())
-    points = int(entryPoints.get())
+    try:
+        score = int(entryScore.get())
+        points = int(entryPoints.get())
+    except ValueError:
+        output_label.config(text="Please enter valid numbers.")
+        return
 
     course.add_assignment(name, score, points)
 
 tk.Button(root, text="Add Assignment", command=add_assignment_gui).pack()
+
+
+output_label = tk.Label(root, text="", justify="left", anchor="w")
+output_label.pack()
+
+
+def show_assignments():
+    course = get_selected_course()
+    result = course.display_course_assignments()
+    output_label.config(text=result)
+
+tk.Button(root, text="Show Assignments", command=show_assignments).pack()
+
+def show_grade():
+    course = get_selected_course()
+    result = course.display_course_grade()
+    output_label.config(text=result)
+
+tk.Button(root, text="Show Grade", command=show_grade).pack()
+
+def clear_fields():
+    entryName.delete(0, tk.END)
+    entryScore.delete(0, tk.END)
+    entryPoints.delete(0, tk.END)
+
+tk.Button(root, text="Clear Fields", command=clear_fields).pack()
+
+def save_data_gui():
+    course = get_selected_course()
+    course.save_data()
+
+tk.Button(root, text="Save", command=save_data_gui).pack()
+def load_data_gui():
+    course = get_selected_course()
+    message = course.load_data()
+
+    if message:
+        output_label.config(text=message)
+
+tk.Button(root, text="Load", command=load_data_gui).pack()
+
+tk.Button(root, text="Quit", command=root.quit).pack()
+
 
 
 root.mainloop()
